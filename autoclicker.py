e1b6775221b0054e877bc966bc4ce25da02b1c4a
cypher3215 <140287543+cypher3215@users.noreply.github.com>
2024-06-27 14:46:18 -0500

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -2,17 +2,17 @@ import pyautogui
 import keyboard
 import autoclicker
 
-
+print("Bienvenido maestro San porfavor presione f6 para activar y f7 para desactivar el autoclicker")
 def clicker1():
     presionar = False 
     while True:
-        if keyboard.is_pressed('s') and not presionar:
+        if keyboard.is_pressed('f6') and not presionar:
             presionar = True
             while presionar:
                 pyautogui.click()
                 if keyboard.is_pressed('k'):
                     presionar = False
-        elif keyboard.is_pressed('k'):
+        elif keyboard.is_pressed('f7'):
             presionar = False
 
 clicker1()
